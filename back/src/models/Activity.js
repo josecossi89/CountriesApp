32920c8fd3cbfd63458140b5a23def889e3821c3
Jose Cossi <josecossib@gmail.com>
2022-04-26 21:48:52 -0300

    entities added in model Activity

---
+++ back/src/models/Activity.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("activity", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.ENUM("1", "2", "3", "4", "5"),
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+      type: DataTypes.ENUM("Summer", "Autum", "Winter", "Spring"),
+      allowNull: false,
+    },
+  });
+};
